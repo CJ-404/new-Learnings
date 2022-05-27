d9866cd319e1116f2c6a86447160df5b7c0ec15b
CJ-404 <cj.ptsz@yahoo.com>
2022-05-27 14:50:13 +0530

    simplify by removing unnecessary divs

--- get_user_location/style/style.js
+++ get_user_location/style/style.js
@@ -1,30 +1,30 @@
-let hidden_div = document.querySelector(".location");
-let header = document.querySelector("h1");
+let hidden_div = document.querySelector("h1");
+let hidden_dclass = hidden_div.classList;
 var key = API_KEY;
 
 
 document.querySelector(".wrapper").addEventListener("click", function() {
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        hidden_div.innerHTML = "The Browser Does not Support";
+    if (!hidden_dclass.contains("hidden")) {
+        if (navigator.geolocation) {
+            hidden_div.innerHTML = "Allow to detect Location";
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+            hidden_div.innerHTML = "The Browser Does not Support";
+        }
     }
 
-    let hidden_dclass = hidden_div.classList;
-
-    if (hidden_dclass.contains("hidden")) {
-        hidden_dclass.remove("hidden");
-        header.classList.add("hidden");
-    } else {
+    if (!hidden_dclass.contains("hidden")) {
         hidden_dclass.add("hidden");
-        header.classList.remove("hidden");
-
+    } else {
+        hidden_dclass.remove("hidden");
+        hidden_div.innerHTML = "Click To Find Your Location";
     }
 })
 
 function showPosition(position) {
-    // console.log(position);
+    hidden_div.innerHTML = "Detecting your Location . . ."
+        // console.log(position);
     let { latitude, longitude } = position.coords;
     // hidden_div.innerHTML = "Your Location is : " + position.coords.latitude + "<br>Longitude : " + position.coords.longitude;
     fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${API_KEY}`)
@@ -33,7 +33,10 @@ function showPosition(position) {
             // console.log(AllDetails);
             let { city, continent, road, state, postcode, country } = AllDetails;
             // console.log(city, continent, road, state, postcode, country);
-            hidden_div.innerHTML = "<strong>Your Location is :</strong> <br><br>Road :" + road + "<br>City :" + city + "<br>postcode :" + postcode + "<br>state :" + state + "<br>country :" + country + "<br>Continent :" + continent;
+            hidden_div.innerHTML = "<i><strong>Your Location is :</strong></i> <br><br>Road :" + road + "<br>City :" + city + "<br>postcode :" + postcode + "<br>state :" + state + "<br>country :" + country + "<br>Continent :" + continent;
+            // console.table(AllDetails);
+        }).catch(() => { //if any error occured
+            hidden_div.innerHTML = "Something went wrong";
         })
 }
 
